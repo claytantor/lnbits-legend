59954ea15b7d4664da280ab4d8b60f5c0f8440d9
Clay Graham <claytantor@gmail.com>
2021-12-01 09:48:23 -0800

    fix runner

--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
-QUART_APP="lnbits.app:create_app()" QUART_ENV=development QUART_DEBUG=true /home/clay/.pyenv/shims/quart assets
-QUART_APP="lnbits.app:create_app()" QUART_ENV=development QUART_DEBUG=true  /home/clay/.pyenv/shims/quart migrate
-/home/clay/.pyenv/shims/hypercorn -k trio --bind clay-deeporb-20.local:5000 'lnbits.app:create_app()'
+QUART_APP="lnbits.app:create_app()" QUART_ENV=development QUART_DEBUG=true quart assets
+QUART_APP="lnbits.app:create_app()" QUART_ENV=development QUART_DEBUG=true quart migrate
+hypercorn -k trio --bind 0.0.0.0:5000 'lnbits.app:create_app()'
