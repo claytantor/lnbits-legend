40b21fab44db8adf9a1f65d2b94af405ff099104
Clay Graham <claytantor@gmail.com>
2022-04-15 09:58:39 -0700

    trying to normalize for dev

--- scripts/cicd/backup_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ENV=$1
-TS=$(date "+%Y-%m-%d_%H-%M-%S")
-TFILE="lnbits-legend_data_$ENV-$TS.tar.gz"
-tar -czvf $TFILE ./data
-aws s3 cp $TFILE s3://$ENV.devops.rapaygo/lnbits/data/$TFILE
-rm $TFILE
--- scripts/cicd/build_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# cp -rvf $(pwd)/../lnbits-ext-rapaygo/rapaygo lnbits/extensions/.
-docker build --no-cache -t claytantor/lnbits-legend:latest . 
